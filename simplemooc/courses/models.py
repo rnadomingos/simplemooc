49c313bb70fab47b80df8862ce02e499a6e23658
Renan Domingos <rna.domingos@hotmail.com>
2020-08-11 15:27:27 -0300

    c6 - Admin Django

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+
+#MODO SIMPLES DE USAR QUERY_SET ** SEPARANDO O *_ICONTENS POR VIRGULA, O DJANGO CONSIDERA AND NA QUERY
+    # class CourseManager (models.Manager):
+    # def search(self, query):
+    #     return self.get_queryset().filter(
+    #         name_icontains=query, description_icontains=query
+    #         ) 
+
+class CourseManager (models.Manager):
+        
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | \
+            models.Q(description__icontains=query)
+        )
+
+
 class Course(models.Model):
 
     name = models.CharField(("Nome"), max_length=100)
@@ -11,3 +28,10 @@ class Course(models.Model):
     image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
     create_at = models.DateTimeField('Criado em', auto_now_add=True)
     update_at = models.DateTimeField('Alterado em', auto_now=True)
+
+
+    objects = CourseManager()
+
+    def __str__(self):
+        return self.name
+    
