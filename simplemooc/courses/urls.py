c0f1737e04ce1cce9dd0ca88af4255d61adc9d29
Renan Domingos <rna.domingos@hotmail.com>
2020-08-21 20:41:17 -0300

    c10 - correcao urls.py

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls import url
 
 app_name='courses'
 
 urlpatterns = [
+
+
         #path('', views.home, name='home'),
         #path('contato/', views.contact, name='contact'),
+        #url(r'^$', views.index, name='index'), ALTERNATIVO
         path('', views.index, name='index'),
 
 ]
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -13,16 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#from django.contrib import admin
+#from django.urls import path, include
+#from simplemooc.core import urls, views
+#from simplemooc.courses import urls, views
+
 from django.contrib import admin
 from django.urls import path, include
-from simplemooc.core import urls
-from simplemooc.courses import urls
+from simplemooc.core import views, urls
+from simplemooc.courses import views, urls
+from django.conf import settings
+#from django.conf.urls.static import static
+
+
+
+#urlpatterns = [
+#        path('', include(urls,namespace='core')),
+#        path('cursos/', include(urls,namespace='courses')),
+#        path('admin/', admin.site.urls),
+#]
 
 urlpatterns = [
-        path('', include(urls,namespace='core')),
-        path('cursos/', include(urls,namespace='courses')),
-        path('admin/', admin.site.urls),
-        
-
-]
-
+	path('', include('simplemooc.core.urls', namespace='Core')),
+    path('curso/', include('simplemooc.courses.urls', namespace='Courses')),
+  #  path('conta/', include('simplemooc.accounts.urls', namespace='Accounts')),
+  #  path('forum/', include('simplemooc.forum.urls', namespace='Forum')),
+	
+    path('admin/', admin.site.urls),
+] 
