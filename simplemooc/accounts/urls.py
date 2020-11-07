a053ba37b3bc2348d158b372d6975a56093ad62c
Renan Domingos <rna.domingos@hotmail.com>
2020-11-07 17:10:47 -0300

    c15 - Painel-Deshboard

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -7,7 +7,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 app_name='accounts'
 
 urlpatterns = [
+        url(r'^$', views.dashboard, name='dashboard'),
         url(r'^entrar/$', LoginView.as_view(), name='login'), 
         url(r'^cadastre-se/$', views.register, name='register'), 
+        url(r'^sair/$', LogoutView.as_view(next_page='core:home'), name='logout'),
+        url(r'^editar$', views.edit, name='edit'), 
 
-]       
+]               
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
+
+
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
 
 def register(request):
     templete_name = 'accounts/register.html'
@@ -9,7 +17,13 @@ def register(request):
         #form = UserCreationForm(request.POST) ***Aula Login inicial usando o forms padrão do user creation form***
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            #form.save()
+            user = form.save()
+            user = authenticate(
+                username=user.username, password=form.cleaned_data['password1']
+                )
+            login(request, user)
+            return redirect('core:home')
             return redirect(settings.LOGIN_URL)
     else: 
         #form = UserCreationForm() ***Aula Login inicial usando o forms padrão do user creation form***
@@ -18,3 +32,12 @@ def register(request):
         'form': form
     }
     return render(request, templete_name, context)
+
+
+@login_required
+def edit(request):
+    template_name = 'accounts/edit.html'
+    form = EditAccountForm()
+    context = {}
+    context['form'] = form
+    return render(request, template_name, context)
