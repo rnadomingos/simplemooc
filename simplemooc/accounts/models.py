912da2c6914fb75e7e6e9d9c1ac6678cac9cbc44
Renan Domingos <rna.domingos@hotmail.com>
2020-11-15 08:40:44 -0300

    c17 - Criando Models Password Reset

--- simplemooc/accounts/models.py
+++ simplemooc/accounts/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.core import validators
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
     UserManager)
+from django.conf import settings
 
 class User(AbstractBaseUser, PermissionsMixin):
 
@@ -36,4 +37,23 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
 
+class PasswordReset(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=("Usuário"),
+            #on_delete=models.CASCADE 
+            #related_name='reset'
+        )
     
+    key = models.CharField(("Chave"), max_length=100, unique=True)
+    created_at = models.DateTimeField(("Criado em"), auto_now_add=True)
+    confirmed = models.BooleanField(("Confirmado?"), default=False, blank=True)
+    
+    def __str__(self):
+        return '{0} em (1}'.format(self.user, self.created_at)
+
+    class Meta:
+        verbose_name = 'Nova Senha'
+        verbose_name_plural = 'Novas Senhas'
+        ordering = ['-created_at']
+    
+
